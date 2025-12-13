25d0e62c743e1da677543c148c52bda5a481a442
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2025-12-13 00:46:03 +0000

    refactor: ajustando a estrutura de importações e tipagem para melhor legibilidade

--- trab2bim4/financApp/src/pages/Home/index.tsx
+++ trab2bim4/financApp/src/pages/Home/index.tsx
@@ -1,59 +1,58 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { TouchableOpacity, Modal } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Modal, TouchableOpacity } from 'react-native';
 
-import { AuthContext } from '../../contexts/auth'
 
-import Header from '../../components/Header';
-import { 
-  Background, 
-  ListBalance,
+import {
   Area,
-  Title,
-  List
- } from './styles'; 
+  Background,
+  List,
+  ListBalance,
+  Title
+} from './styles';
 
-import api from '../../services/api'
 import { format } from 'date-fns';
+import api from '../../services/api';
 
 import { useIsFocused } from '@react-navigation/native';
 import BalanceItem from '../../components/BalanceItem';
+import CalendarModal from '../../components/CalendarModal';
 import HistoricoList from '../../components/HistoricoList';
-import CalendarModal from '../../components/CalendarModal'
+import { Balance, Movement } from '../../types';
 
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
-export default function Home(){
+export default function Home() {
   const isFocused = useIsFocused();
-  const [listBalance, setListBalance] = useState([]);
-  const [movements, setMovements] = useState([]);
+  const [listBalance, setListBalance] = useState<Balance[]>([]);
+  const [movements, setMovements] = useState<Movement[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
 
   const [dateMovements, setDateMovements] = useState(new Date())
 
 
-  useEffect(()=>{
+  useEffect(() => {
     let isActive = true;
 
-    async function getMovements(){
+    async function getMovements() {
 
       let date = new Date(dateMovements)
       let onlyDate = date.valueOf() + date.getTimezoneOffset() * 60 * 1000;
       let dateFormated = format(onlyDate, 'dd/MM/yyyy');
 
       const receives = await api.get('/receives', {
-        params:{
+        params: {
           date: dateFormated
         }
       })
 
       const balance = await api.get('/balance', {
-        params:{
-          date: dateFormated 
+        params: {
+          date: dateFormated
         }
       })
 
-      if(isActive){
+      if (isActive) {
         setMovements(receives.data)
         setListBalance(balance.data);
       }
@@ -66,40 +65,39 @@ export default function Home(){
   }, [isFocused, dateMovements])
 
 
-  async function handleDelete(id){
-    try{
+  async function handleDelete(id) {
+    try {
       await api.delete('/receives/delete', {
-        params:{
+        params: {
           item_id: id
         }
       })
 
       setDateMovements(new Date())
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
   }
 
-  function filterDateMovements(dateSelected){
+  function filterDateMovements(dateSelected) {
     // console.log(dateSelected);
     setDateMovements(dateSelected);
   }
 
 
-  return(
+  return (
     <Background>
-      <Header title="Minhas movimentações" />
 
       <ListBalance
         data={listBalance}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
-        keyExtractor={ item => item.tag }
-        renderItem={ ({ item }) => ( <BalanceItem data={item} /> )}
+        keyExtractor={item => item.tag}
+        renderItem={({ item }) => (<BalanceItem data={item} />)}
       />
 
       <Area>
-        <TouchableOpacity onPress={ () => setModalVisible(true) }>
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
           <Icon name="event" color="#121212" size={30} />
         </TouchableOpacity>
         <Title>Ultimas movimentações</Title>
@@ -107,15 +105,15 @@ export default function Home(){
 
       <List
         data={movements}
-        keyExtractor={ item => item.id }
-        renderItem={ ({ item }) => <HistoricoList data={item} deleteItem={handleDelete} />  }
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <HistoricoList data={item} deleteItem={handleDelete} />}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 20 }}
       />
 
       <Modal visible={modalVisible} animationType="fade" transparent={true}>
         <CalendarModal
-          setVisible={ () => setModalVisible(false) }
+          setVisible={() => setModalVisible(false)}
           handleFilter={filterDateMovements}
         />
       </Modal>
