6a9adf1a3a46eddca97217b53c311b3238bf4a5a
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2025-12-13 01:15:37 +0000

    refactor: convertendo para ts

--- trab2bim4/financApp/src/types/index.ts
+++ trab2bim4/financApp/src/types/index.ts
@@ -1,26 +1,26 @@
 export interface User {
-    id: string;
-    name: string;
-    email: string;
-    balance: number;
+  id: string;
+  name: string;
+  email: string;
+  balance: number;
 }
 
 export interface Movement {
-    id: string;
-    description: string;
-    value: number;
-    type: 'receita' | 'despesa';
-    date: string;
-    user_id: string;
+  id: string;
+  description: string;
+  value: number;
+  type: 'receita' | 'despesa';
+  date: string;
+  user_id: string;
 }
 
 export interface Balance {
-    tag: 'saldo' | 'receita' | 'despesa';
-    saldo: number;
+  tag: 'saldo' | 'receita' | 'despesa';
+  saldo: number;
 }
 
 export interface LoginResponse {
-    id: string;
-    name: string;
-    token: string;
+  id: string;
+  name: string;
+  token: string;
 }
